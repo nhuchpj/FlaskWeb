d7914dfcf61745882d3f18409b99009128182d66
chpj <15647166@qq.com>
2019-02-17 22:20:59 +0800

    3 Bootstrap

--- hello.py
+++ hello.py
@@ -1,35 +1,28 @@
 from flask import Flask, render_template
-from flask_script import Manager
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
-manager = Manager(app)
+bootstrap = Bootstrap(app)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
 
 @app.route('/')
 def index():
-    # return '<h1>Hello World!</h1>'
     return render_template('index.html')
 
-    # from flask import make_response
-    # response = make_response('<h1> This document carries a cookie! </h1>')
-    # response.set_cookie('answer', '42')
-    # return response
 
-    # from flask import redirect
-    # return redirect('http://www.baidu.com')
-
-
-@app.route('/user/<user>')
-def user(user):
-    return render_template('user.html', name=user)
-    # return '<h1> Hello, {}! </h1>'.format(name)
-
-    # from flask import abort
-    # user = False
-    # if not user:
-    #     abort(404)
-    # return '<h1> Hello, %s! </h1>' % name
-
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
 
 if __name__ == '__main__':
-    app.run(port = 8000, debug = True)
-    # manager.run()
+    bootstrap.
